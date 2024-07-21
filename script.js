6687c565000c3b6b086e1435e82b5027437cb482
mateoo04 <144594500+mateoo04@users.noreply.github.com>
2024-07-21 15:29:11 +0200

    Add cards which display book details

--- script.js
+++ script.js
@@ -12,9 +12,38 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary(book) {
-    if (book instanceof Book) { 
-        myLibrary.push(book); 
+    if (book instanceof Book) {
+        myLibrary.push(book);
     }
 }
 
 addBookToLibrary(new Book('hey', 'its me', 300, true));
+addBookToLibrary(new Book('hey', 'its me', 300, true));
+addBookToLibrary(new Book('hey', 'its me', 300, true));
+addBookToLibrary(new Book('hey', 'its me', 300, true));
+
+const main = document.querySelector('main');
+
+function displayBooks() {
+    for (const book of myLibrary) {
+        let item = document.createElement('div');
+        item.classList.add('item');
+
+        let titleText = document.createElement('h2');
+        titleText.textContent = book.title;
+
+        let authorText = document.createElement('p');
+        authorText.textContent = book.author;
+
+        let pagesText = document.createElement('p');
+        pagesText.textContent = `${book.pages} pages`;
+
+        item.appendChild(titleText);
+        item.appendChild(authorText);
+        item.appendChild(pagesText);
+
+        main.appendChild(item);
+    }
+}
+
+displayBooks();
