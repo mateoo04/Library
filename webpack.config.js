cb0ae0baac522c605fcca7e19350cead21916070
mateoo04 <144594500+mateoo04@users.noreply.github.com>
2024-08-09 19:42:34 +0200

    Implement use of Webpack and other plugins

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/script.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    watchFiles: ['src/*.*'],
+  },
+};
